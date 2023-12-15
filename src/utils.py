ccc011c093af126a5265e0e49693ade55421d23b
Samuel Atkins <s.m.atkins73@gmail.com>
2023-12-14 18:34:08 -0800

    Updates model loading to utilize the device correctly during inference and updates tatooine.zip

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,8 @@ def load_model(model: nn.Module, device: Union[str, int], filename: str = None):
     loaded_model = "models/" + filename if filename is not None else None
     if loaded_model is not None:
         print("Loading model...")
-        model.load_state_dict(torch.load(loaded_model))
+        model.load_state_dict(
+            torch.load(loaded_model, map_location=torch.device(device))
+        )
         return model.to(device)
     return model
